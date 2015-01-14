6e7632561aba3ad69a84e9a2fa04b6f08713173f
Rob Cary <robcary91@gmail.com>
2015-01-14 13:29:41 -0500

    Second commit. Added review page, more CSS changes

---
+++ review.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Drunk At The Jazz Club</title>
+	<link type="text/css" rel="stylesheet" href="main.css"/>
+</head>
+<body style="background-color:#426352">
+	<div id="mainnavbar">
+		<table>
+			<thead></thead>
+			<tbody>
+				<tr class="mainnavbutton">
+					<td><a href="index.php">(home.)</a></td>
+				</tr>
+				<tr class="mainnavbutton">
+					<td><a href="review.php">(reviews.)</a></td>
+				</tr>
+				<tr class="mainnavbutton">
+					<td>(database.)</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+	<div id="mainpageintro">
+		<h1 class="subtext">We talk about music...</h1>
+		<p style="text-align:left">Welcome to the Jazz Club. Drinks are on us. Always. 
+			Take a seat, order something, kick back. Feet off the tables though. 
+			Tonight we will be talking, singing, musing, reflecting, and perhaps most importantly, drinking.</p>
+		<p>"You could have been anywhere in the world tonight, but you're here with us. And we appreciate that..."</p>
+	</div>
+</body>
+</html>
