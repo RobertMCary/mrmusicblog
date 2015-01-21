3ddcb8d3a8f79499be2b42aa2f352b225e6a9724
Rob Cary <robcary91@gmail.com>
2015-01-21 13:49:33 -0500

    Added basic user auth, login availability coming soon

--- admin.php
+++ admin.php
@@ -1,3 +1,13 @@
+<?php
+	/* Begin session */
+	session_start();
+
+	/* Set form token */
+	$form_token = md5( uniqid('auth', true) );
+
+	/* Set session form token */
+	$_SESSION['form_token'] = $form_token;
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -26,8 +36,25 @@
 	</div>
 	<div id="mainpageintro">
 		<h1 class="subtext">Administrator panel.</h1>
-		<p style="text-align:left">This area of the site allows an admin (and <em>only</em> an admin!) to add
-			, edit, and delete various content on the site, the least of which is the reviews.</p>
+		<h2>Add user</h2>
+		<form action="admin_submit.php" method="post">
+		<fieldset>
+		<p>
+			<label for="mb_username">Username</label>
+			<input type="text" id="mb_username" name="mb_username" value="" maxlength="20" />
+		</p>
+		<p>
+			<label for="mb_password">Password</label>
+			<input type="text" id="mb_password" name="mb_password" value="" maxlength="20" />
+		</p>
+		<p>
+			<input type="hidden" name="form_token" value="<?php echo $form_token; ?>" />
+			<input type="submit" value="Submit" />
+		</p>
+		</fieldset>
+		</form>
+		<!--<p style="text-align:left">This area of the site allows an admin (and <em>only</em> an admin!) to add
+			, edit, and delete various content on the site, the least of which is the reviews.</p>-->
 	</div>
 </body>
 </html>
---
+++ admin_submit.php
@@ -0,0 +1,102 @@
+<?php
+/* Begin session */
+session_start();
+
+/* Check that both the username, password and form token have been sent */
+if(!isset( $_POST['mb_username'], $_POST['mb_password'], $_POST['form_token']))
+{
+    $message = 'Please enter a valid username and password';
+}
+/* Check form token validity */
+elseif( $_POST['form_token'] != $_SESSION['form_token'])
+{
+    $message = 'Invalid form submission';
+}
+/* Check the username for correct length */
+elseif (strlen( $_POST['mb_username']) > 20 || strlen($_POST['mb_username']) < 4)
+{
+    $message = 'Incorrect length for username';
+}
+/* Check the password for correct length */
+elseif (strlen( $_POST['mb_password']) > 20 || strlen($_POST['mb_password']) < 4)
+{
+    $message = 'Incorrect length for password';
+}
+/* Check that the username has only alphanumeric characters */
+elseif (ctype_alnum($_POST['mb_username']) != true)
+{
+    /* if there is no match... */
+    $message = "Username must be alpha numeric";
+}
+/* Check the password has only alpha numeric characters */
+elseif (ctype_alnum($_POST['mb_password']) != true)
+{
+        /* if there is no match... */
+        $message = "Password must be alpha numeric";
+}
+else
+{
+    /* At this point, data must be valid, and we can insert into the database */
+    $mb_username = filter_var($_POST['mb_username'], FILTER_SANITIZE_STRING);
+    $mb_password = filter_var($_POST['mb_password'], FILTER_SANITIZE_STRING);
+
+    /* Encrypt the password */
+    $mb_password = sha1( $mb_password );
+    
+    /* Connect to db*/
+    $mysql_hostname = 'localhost';
+
+    $mysql_username = 'root';
+
+    $mysql_password = 'c1u2r3r4y';
+
+    $mysql_dbname = 'dbMusicBlog_Auth';
+
+    try
+    {
+        $dbh = new PDO("mysql:host=$mysql_hostname;dbname=$mysql_dbname", $mysql_username, $mysql_password);
+        /* $message = a message saying we have connected */
+
+        /* Set error mode to exceptions */
+        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        /* Prepare insert */
+        $stmt = $dbh->prepare("INSERT INTO tblUsers (mb_username, mb_password ) VALUES (:mb_username, :mb_password )");
+
+        /* Bind parameters */
+        $stmt->bindParam(':mb_username', $mb_username, PDO::PARAM_STR);
+        $stmt->bindParam(':mb_password', $mb_password, PDO::PARAM_STR, 40);
+
+        /* Execute statement */
+        $stmt->execute();
+
+        /* Unset the form token session variable */
+        unset( $_SESSION['form_token'] );
+
+        /* If all is done, say thanks */
+        $message = 'New user added';
+    }
+    catch(Exception $e)
+    {
+        /* Check if the username already exists */
+        if( $e->getCode() == 23000)
+        {
+            $message = 'Username already exists';
+        }
+        else
+        {
+            /* If we are here, something has gone wrong with the database */
+            $message = 'We are unable to process your request. Please try again later"';
+        }
+    }
+}
+?>
+
+<html>
+<head>
+<title>PHPRO Login</title>
+</head>
+<body>
+<p><?php echo $message; ?></p>
+</body>
+</html>
