53c6cfa53c1425a487050df11c865298c85755ad
Rob Cary <robcary91@gmail.com>
2015-01-27 15:38:55 -0500

    SQL works! Added a review page for displaying reviews, currently tweaking CSS

--- article.php
+++ article.php
@@ -19,16 +19,30 @@
     			die("Connection failed: " . $conn1->connect_error);
 			} 
 
-			$sql = "SELECT reviewAuthor FROM tblReviews";
-			$result = $conn1->query($sql);
-			if ($result_->num_rows > 0){
-				while($row = $result->fetch_assoc()){
-					echo "<h1>" . $row["reviewAuthor"] . "</h1>";
-				}
-			} else {
-				echo "0 results.";
+			$sql = "SELECT recordProducer, recordTitle FROM tblReviews";
+			foreach($conn1->query($sql) as $row){
+				echo "<p class='review_title'>" . $row['recordProducer'] . " // " . $row['recordTitle'] . "</p>" . "<br/>";
 			}
 		?>
+		<div id="review_text_div"><p style="text-indent: 1.5em">Lorem ipsum dolor sit amet, pro sapientem dissentiet ad, at quis conclusionemque eum. Ex erat dicat quo, usu clita hendrerit te. Ipsum dicit nam ne, viris verear recteque mei te. Lobortis erroribus mel ex, per ne ubique dissentias delicatissimi.
+
+Nulla homero saperet sed ne, pri te falli omnes ocurreret. Nam atqui ubique tincidunt et, an brute sadipscing duo, sea sint tation nostro id. An vitae petentium mel. Cu hendrerit democritum mea.
+
+Eum erant feugiat volumus ex. Id duo legimus postulant principes, dolore qualisque ne usu. Te pri fugit officiis recteque, ei sit mucius tincidunt. Vitae reprehendunt te nam, at prima vitae nonumes eos. Ei vim graeci torquatos voluptaria, veritus alienum eam no, in vel facer semper. Sed ex mutat minimum theophrastus, velit quodsi deterruisset vel et. Sea offendit delectus et, graece labore delenit vix eu.
+
+Impedit delicatissimi ei vel, eu ancillae similique maiestatis pri. Ei eum dolorem pertinacia. Duo affert nonumy suavitate in, duo ad summo tractatos laboramus, an sale eleifend volutpat qui. Virtute equidem ex sit, nostrum nominavi vim ne, odio vero aeterno quo ea.
+
+Et iudico repudiare mel, in per oratio electram imperdiet, pericula incorrupte sit ea. Cu duo elitr fabulas suscipiantur, ex tantas fierent vituperata mel. An duo porro salutandi deterruisset. Pri modo putent vulputate ea, cum ei fabellas dignissim interesset. Nonumes consetetur pri et.</p>
+		</div>
+		<div id="review_info_div">
+			<?
+				$sql = "SELECT recordLabel,  recordReleaseDate FROM tblReviews";
+				foreach($conn1->query($sql) as $row){
+					echo "<h3>Label: " . $row['recordLabel'] . "</h3><br/>";
+					echo "<h4>Released on: " . $row['recordReleaseDate'] . "</h4>";
+				}
+			?>
+		</div>
 	</div>
 <?php include("mainnavbar.php"); ?>
 </body>
